fd351f8e689ee835a2f95e6cdc77d8970c5b76f7
Ivan <sheva134@mail.ru>
2023-03-29 14:29:04 +0300

    выполнение дз

---
+++ task/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int InputNumber(string str)
+{
+    System.Console.Write(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int M = InputNumber ("Введите значение М: "); 
+int N = InputNumber ("Введите значение N: "); 
+int sum = 0;
+
+     for (int i = M; i <= N; i++) 
+    {
+        sum += i;
+    }
+
+System.Console.WriteLine($"Сумма натуральных чисел от {M} до {N} равна: {sum}");
+
---
+++ task1/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program 
+{
+    static int Ackermann(int m, int n) {
+        if (m == 0) 
+        {
+            return n + 1;
+        }
+        else if (n == 0) 
+        {
+            return Ackermann(m - 1, 1);
+        }
+        else 
+        {
+            return Ackermann(m - 1, Ackermann(m, n - 1));
+        }
+    }
+
+    static void Main() 
+    {
+        Console.Write("Введите количество пар чисел: ");
+        int count = int.Parse(Console.ReadLine());
+
+       int[,] pairs = new int[count, 2];
+
+        Console.WriteLine("Введите пары чисел через пробел:");
+
+        for (int i = 0; i < count; i++) 
+        {
+            Console.Write($"Пара {i + 1} (m n): ");
+            string[] input = Console.ReadLine().Split(' ');
+            pairs[i, 0] = int.Parse(input[0]);
+            pairs[i, 1] = int.Parse(input[1]);
+        }
+
+        Console.WriteLine("Результаты:");
+
+        for (int i = 0; i < count; i++) 
+        {
+            int m = pairs[i, 0];
+            int n = pairs[i, 1];
+            int result = Ackermann(m, n);
+            Console.WriteLine($"A({m}, {n}) = {result}");
+        }
+    }
+}
